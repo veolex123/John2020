892c4476e7a786e8f1691d0d092fcb8fba0761f2
John <638053@ibsh.tw>
2020-09-18 17:18:48 +0800

    hw3

--- hello09021.py
+++ hello09021.py
@@ -1,6 +1,10 @@
-name = input(' ')
-http =' '
-if http in name :
-    print('yes http')
-else:
-    print('no http')
+l = int(input("enter the lower interval"))
+u = int(input("enter the upper interval"))
+for num in range(l,u+1):
+    if num>1:
+        for i in range(2,num):
+            if(num%i)==0:
+             break
+        else:
+             print(num)
+        
